1da2a433961a3c7640068a3390a43730b7179e1e
Alberto Sierra <alberto.sierra@murciaeduca.es>
2017-04-25 10:15:19 +0200

    Rol dinámico para que el creador de una pregunta pueda borrar comentarios asociados

--- common/models/quiz.js
+++ common/models/quiz.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = function(Quiz) {
-
+	Quiz.observe('before save', function (ctx, next) {
+		if (ctx.isNewInstance) {
+			if (ctx.instance) {
+				ctx.instance.userId = ctx.options && ctx.options.accessToken && ctx.options.accessToken.userId;
+			} else {
+				delete ctx.data.verificado;
+			}
+		}
+		next();
+	});
 };
