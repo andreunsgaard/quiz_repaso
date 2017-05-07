8219eac646f77df6a429f4f28898d98d286492eb
Alberto Sierra <alberto.sierra@murciaeduca.es>
2017-05-07 20:35:04 +0200

    El ejemplo completo con todos

---
+++ client/js/todo.js
@@ -0,0 +1,41 @@
+// Copyright IBM Corp. 2015. All Rights Reserved.
+// Node module: loopback-example-angular
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+angular
+  .module('app')
+  .controller('TodoController', ['$scope', '$state', 'Puntuacion', function($scope,
+      $state, Puntuacion) {
+    $scope.todos = [];
+    function getPuntuacions() {
+      Puntuacion
+        .find()
+        .$promise
+        .then(function(results) {
+          $scope.todos = results;
+        });
+    }
+    getPuntuacions();
+
+    $scope.addPuntuacion = function() {
+      Puntuacion
+        .create($scope.newPuntuacion)
+        .$promise
+        .then(function(todo) {
+          $scope.newPuntuacion = '';
+          $scope.todoForm.content.$setPristine();
+          $('.focus').focus();
+          getPuntuacions();
+        });
+    };
+
+    $scope.removePuntuacion = function(item) {
+      Puntuacion
+        .deleteById(item)
+        .$promise
+        .then(function() {
+          getPuntuacions();
+        });
+    };
+  }]);
